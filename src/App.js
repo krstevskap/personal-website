ee04e2f29723fc9e29787d360e689d4f7b27d426
krstevskap <krstevskap13@gmail.com>
2024-11-25 15:43:21 +0100

    Add animation on scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
 import './App.css';
+import 'aos/dist/aos.css';
 import About from './components/About/About';
 import Homepage from './components/Homepage/Homepage';
 import Projects from './components/Projects/Projects';
 
 function App() {
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-out',
+      once: false,
+      startEvent: "scroll",
+    });
+  }, []);
+
   return (
     <div className="App">
       <Homepage />
