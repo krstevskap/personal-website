02b7c878c9a9d646d11bc2a3b2c4f46b8ac27fde
krstevskap <krstevskap13@gmail.com>
2025-01-18 22:41:37 +0100

    Add  new project to cards

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,6 +19,16 @@ const Projects = () => {
 
         <div className="card">
           <Card
+            description="A React-based web application that allows users to track their income, expenses, and categorize them."
+            link="https://github.com/krstevskap/expense-tracker"
+            technologies="React, Firebase"
+            title="Expense Tracker"
+            website="https://expense-tracker-497b9.web.app"
+          />
+        </div>
+
+        <div className="card">
+          <Card
             description="A React-based task management application that allows users to create, edit, delete, and drag-and-drop tasks between columns. The app features persistent data storage using localStorage. "
             link="https://github.com/krstevskap/kanban-board"
             technologies="React"
